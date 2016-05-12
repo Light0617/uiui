bf2997f73b427fdbe06a7c7f175615e6e4cc4236
tokumura <takeshi.okumura.gv@hitachi.com>
2016-05-13 06:58:36 +0900

    NEWRAIN-4056: In the list view on Storage systems summary IPaddress is not listed

--- app/scripts/controllers/storage-systems.js
+++ app/scripts/controllers/storage-systems.js
@@ -147,6 +147,15 @@ angular.module('rainierApp')
 
                 },
                 {
+                    title: 'storage-systems-svp-ip-address',
+                    sizeClass: 'sixth',
+                    sortField: 'svpIpAddress',
+                    getDisplayValue: function (item) {
+                        return item.svpIpAddress;
+                    }
+
+                },
+                {
                     title: 'storage-systems-model',
                     sizeClass: 'sixth',
                     sortField: 'model',
