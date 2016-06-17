cf4eff288e61b503d3d69eeab946f136e94d2ac9
Yasuda <yu.yasuda.jz@ps.hitachi-solutions.com>
2016-06-17 11:08:50 +0900

    NEWRAIN-2703: UI Pre-submit validation - Create Storage System

--- app/scripts/controllers/storage-systems-add.js
+++ app/scripts/controllers/storage-systems-add.js
@@ -27,9 +27,9 @@ angular.module('rainierApp')
             },
             isInvalid: function() {
                 return !$scope.model ||
-                    _.isEmpty($scope.model.svpIpAddress) ||
-                    _.isEmpty($scope.model.username) ||
-                    _.isEmpty($scope.model.password) ;
+                    $scope.model.validationForm.svpIpAddress.$invalid ||
+                    $scope.model.validationForm.username.$invalid ||
+                    $scope.model.validationForm.password.$invalid ;
             }
         };
     });
