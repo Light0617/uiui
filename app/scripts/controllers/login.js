adc56f55d6c841309db46506ad6bc302e5b7e3f6
chuck <chikura.hara.fb@hitachi.com>
2016-06-19 19:31:01 +0900

    use login bel-ui directive

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,30 +8,28 @@
  * Controller of the rainierApp
  */
 angular.module('rainierApp')
-    .controller('LoginCtrl', function($scope, $location, authService) {
+    .controller('LoginCtrl', function ($scope, $compile, $location, authService, synchronousTranslateService) {
         $scope.model = {
-            date: new Date(),
-            username: '',
-            password: '',
-            loginDisabled: function() {
-                return $scope.model.username.length === 0 || $scope.model.password.length === 0;
-            },
-            login: function() {
-
+            company: synchronousTranslateService.translate('brand-company'),
+            product: synchronousTranslateService.translate('brand-rainier'),
+            submitAction: function () {
                 authService.login($scope.model.username, $scope.model.password).then(
-                    function() {
+                    function () {
                         $location.path('/');
                     },
-                    function(res){
-                        if (res && res.status === 401){
+                    function (res) {
+                        if (res && res.status === 401) {
                             $scope.model.unauthorized = true;
                         } else {
                             $scope.model.loginFailed = true;
                         }
                     }
                 );
-
-            }
-
+            },
+            userPlaceholder: synchronousTranslateService.translate('login-username-placeholder'),
+            passwdPlaceholder: synchronousTranslateService.translate('login-password-placeholder'),
+            failedMessage: synchronousTranslateService.translate('login-common-error'),
+            unauthorizedMessage: synchronousTranslateService.translate('login-unauthorized-error'),
+            palette: ['#35665C', '#458B74', '#56AF8C', '#66D4A4']
         };
     });
