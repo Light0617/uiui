42d56a8c6c016dc011c3368107b7184bd01e8c89
Shigeki Arata <shigeki.arata@hds.com>
2018-02-05 20:50:30 +0900

    NEWRAIN-8118:Modify storage advisor embedded launch

---
+++ app/scripts/services/storage-advisor-embedded-session-service.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('rainierApp')
+    .factory('storageAdvisorEmbeddedSessionService', function ($window, $modal, synchronousTranslateService, modalDialogService) {
+        return {
+            getLaunchUrl: function (storageSystemId) {
+                return {
+                    type: 'link',
+                    title :'storage-system-launch-hsae',
+                    onClick: function (orchestratorService) {
+                        // Open a new tab with empty url
+                        var redirectWindow = $window.open('', '_blank');
+                        redirectWindow.document.write(synchronousTranslateService.translate('hsae-session-loding'));
+                        orchestratorService.storageAdvisorEmbeddedUrl(storageSystemId).then(function(result){
+                            // Update the url of the opened tab with storage navigator session url
+                            var url = result.launchServletUrl;
+                            redirectWindow.location.href = url;
+                        }, function (error) {
+                            redirectWindow.document.body.innerText = synchronousTranslateService.translate('hsae-session-html-error');
+                            modalDialogService.showDialog('', 'hsae-session-modal-error', 'warning');
+                        });
+                    }
+                };
+            }
+        };
+    });
--- app/scripts/services/storage-navigator-session-service.js
+++ app/scripts/services/storage-navigator-session-service.js
@@ -8,38 +8,7 @@
  * Factory in the rainierApp.
  */
 angular.module('rainierApp')
-    .factory('storageNavigatorSessionService', function ($q, $window, $modal, synchronousTranslateService) {
-        var showDialog = function (title, message, type)
-        {
-            var defer = $q.defer();
-
-            var modalInstance = $modal.open({
-                templateUrl: 'views/templates/navigator-session-modal.html',
-                windowClass: 'modal confirmation',
-                backdropClass: 'modal-backdrop',
-                controller: function ($scope, $modalInstance)
-                {
-                    $scope.title = title;
-                    $scope.message = message;
-                    $scope.type = type;
-
-                    $scope.ok = function ()
-                    {
-                        modalInstance.close();
-                        defer.resolve();
-                    };
-
-                    $scope.cancel = function ()
-                    {
-                        $modalInstance.dismiss();
-                        defer.reject();
-                    };
-                }
-            });
-
-            return defer.promise;
-        };
-
+    .factory('storageNavigatorSessionService', function ($q, $window, $modal, synchronousTranslateService, modalDialogService) {
         return {
             getNavigatorSessionAction: function (storageSystemId, sessionPageScope) {
                 return {
@@ -48,7 +17,7 @@ angular.module('rainierApp')
                     onClick: function (orchestratorService) {
                         // Open a new tab with empty url
                         var redirectWindow = $window.open('', '_blank');
-                        redirectWindow.document.write('Loading storage navigator...');
+                        redirectWindow.document.write(synchronousTranslateService.translate('navigator-session-loding'));
                         orchestratorService.storageNavigatorSession(storageSystemId, sessionPageScope).then(function(result){
                             // Update the url of the opened tab with storage navigator session url
                             var url = result.launchServletUrl + '/' + result.oneTimeKey;
@@ -61,10 +30,10 @@ angular.module('rainierApp')
                         }, function (error) {
                             if (error.status === 403) {
                                 redirectWindow.document.body.innerText = synchronousTranslateService.translate('navigator-session-permission-html-error');
-                                showDialog('', 'navigator-session-permission-modal-error', 'warning');
+                                modalDialogService.showDialog('', 'navigator-session-permission-modal-error', 'warning');
                             } else {
                                 redirectWindow.document.body.innerText = synchronousTranslateService.translate('navigator-session-html-error');
-                                showDialog('', 'navigator-session-modal-error', 'warning');
+                                modalDialogService.showDialog('', 'navigator-session-modal-error', 'warning');
                             }
                         });
                     }
