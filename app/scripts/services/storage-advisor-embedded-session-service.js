6d7ad2a5fbf54885db5e6856cb686ab68f4ef158
Shigeki Arata <shigeki.arata@hds.com>
2018-02-06 20:31:01 +0900

    NWERAIN-8118:Modify messages and typo

--- app/scripts/services/storage-advisor-embedded-session-service.js
+++ app/scripts/services/storage-advisor-embedded-session-service.js
@@ -10,7 +10,7 @@ angular.module('rainierApp')
                     onClick: function (orchestratorService) {
                         // Open a new tab with empty url
                         var redirectWindow = $window.open('', '_blank');
-                        redirectWindow.document.write(synchronousTranslateService.translate('hsae-session-loding'));
+                        redirectWindow.document.write(synchronousTranslateService.translate('hsae-session-loading'));
                         orchestratorService.storageAdvisorEmbeddedUrl(storageSystemId).then(function(result){
                             // Update the url of the opened tab with storage navigator session url
                             var url = result.launchServletUrl;
--- app/scripts/services/storage-navigator-session-service.js
+++ app/scripts/services/storage-navigator-session-service.js
@@ -17,7 +17,7 @@ angular.module('rainierApp')
                     onClick: function (orchestratorService) {
                         // Open a new tab with empty url
                         var redirectWindow = $window.open('', '_blank');
-                        redirectWindow.document.write(synchronousTranslateService.translate('navigator-session-loding'));
+                        redirectWindow.document.write(synchronousTranslateService.translate('navigator-session-loading'));
                         orchestratorService.storageNavigatorSession(storageSystemId, sessionPageScope).then(function(result){
                             // Update the url of the opened tab with storage navigator session url
                             var url = result.launchServletUrl + '/' + result.oneTimeKey;
