9fc7e218fc86ffdcf63adbd12c9dc6f848d439ac
Sherry Chuang <shanyu.chuang@hitachivantara.com>
2018-03-13 15:14:46 -0700

    Only redraw existing paths when they exist

--- app/scripts/directives/wizard-svg-page.js
+++ app/scripts/directives/wizard-svg-page.js
@@ -728,7 +728,13 @@ angular.module('rainierApp')
                             if (!dataModel) {
                                 return;
                             }
-                            builder._buildTopologicalEditor(d3, selectedSvg, scope, true);
+                            if(dataModel.pathModel.paths.length === 0) {
+                                builder._buildTopologicalEditor(d3, selectedSvg, scope);
+                            }
+                            else {
+                                builder._buildTopologicalEditor(d3, selectedSvg, scope, true);
+
+                            }
                         };
                     });
                 };
