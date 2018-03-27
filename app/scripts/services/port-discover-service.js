b209f48a898d6e06bedf80de2e7773cd66ceffa3
Chikura Hara <chikura.hara.fb@hitachi.com>
2018-03-27 14:50:54 -0700

    NEWRAIN-8304: Discovere structure for unmanaged source storage

--- app/scripts/services/port-discover-service.js
+++ app/scripts/services/port-discover-service.js
@@ -14,6 +14,7 @@ angular.module('rainierApp')
         apiResponseHandlerService,
         Restangular
     ) {
+        var keyForDiscovered = 'hashKey';
         var discoverManagedVolumes = function (
             tgtPortIds,
             srcPortIds,
@@ -29,6 +30,7 @@ angular.module('rainierApp')
             tgtPortIds,
             tgtStorageSystemId
         ) {
+            return discoverGroups(tgtPortIds, tgtStorageSystemId);
         };
 
         var getPortsHash = function (portIds, storageSystemId) {
@@ -39,16 +41,43 @@ angular.module('rainierApp')
 
         };
 
-        var discoverGroups = function (portIds, storageSystemId) {
-            var requests = _.map(portIds, function (pid) { return discoverGroup(pid, storageSystemId); });
-            return $q.all(requests)
-                .then(function (multiLevelDeep) { return _.flatten(multiLevelDeep); })
-                .then(function (singleLevelDeep) {  });
+        var endPoint = function (discoveredLun) {
+            if (!_.isUndefined(discoveredLun.wwn)) {
+                return discoveredLun.wwn;
+            } else if (
+                !_.isUndefined(discoveredLun.iscsiInfo) &&
+                !_.isUndefined(discoveredLun.iscsiInfo.ip) &&
+                !_.isUndefined(discoveredLun.iscsiInfo.iscsiName)
+            ) {
+                return discoveredLun.iscsiInfo.ip + '_' + discoveredLun.iscsiInfo.iscsiName;
+            }
+        };
+
+        var keyOf = function (discoveredLun) {
+            return discoveredLun.portId + discoveredLun.lun + endPoint(discoveredLun);
+        };
+
+        var appendKey = function (discoveredLun) {
+            discoveredLun[keyForDiscovered]= keyOf(discoveredLun);
+            discoveredLun.endPoint = endPoint(discoveredLun);
+            return discoveredLun;
+        };
+
+        var discoveredGroupsToHash = function (discoveredLuns) {
+            return _.chain(discoveredLuns).map(appendKey)
+                .indexBy(keyForDiscovered).value();
         };
 
         var discoverGroup = function (portId, storageSystemId) {
             var url = 'storage-systems/' + storageSystemId + '/storage-ports/' + portId + '/discover-groups';
             return apiResponseHandlerService._apiResponseHandler(Restangular.one(url).post({}))
+        };
+
+        var discoverGroups = function (portIds, storageSystemId) {
+            var requests = _.map(portIds, function (pid) { return discoverGroup(pid, storageSystemId); });
+            return $q.all(requests)
+                .then(_.flatten)
+                .then(discoveredGroupsToHash);
         };
 
         return {
