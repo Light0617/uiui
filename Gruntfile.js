fc4b3f173d239d68148b2059a7dd7e92c61635c5
Yuuki Watanabe <yuki.watanabe@hitachivantara.com>
2018-04-12 12:18:04 +0900

    NEWRAIN-8475:[UI] Fix jshint warning

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,6 +223,7 @@ module.exports = function(grunt) {
         jshint: {
             options: {
                 jshintrc: '.jshintrc',
+                reporterOutput: '',
                 reporter: require('jshint-stylish')
             },
             all: {
