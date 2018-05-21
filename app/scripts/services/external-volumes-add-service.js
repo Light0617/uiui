c07aef5eafccfb5300eb3a9c320ae47f9c096e20
Chikura Hara <chikura.hara.fb@hitachi.com>
2018-05-21 16:41:33 +0900

    9092 more detailed precondition

--- app/scripts/services/external-volumes-add-service.js
+++ app/scripts/services/external-volumes-add-service.js
@@ -171,10 +171,14 @@ angular.module('rainierApp').factory('externalVolumesAddService', function (
         // TODO should rewrite with query
         var filtered = _.chain(ports)
             .filter(function (p) {
-                return _.some(p.attributes, 'EXTERNAL_INITIATOR_PORT');
-            }).filter(function (p) {
+                return _.some(p.attributes, function(a) {
+                    return a === 'External';
+                });
+            })
+            .filter(function (p) {
                 return p.type !== 'FIBRE' || (p.type === 'FIBRE' && p.securitySwitchEnabled);
-            });
+            })
+            .value();
 
         if (filtered.length) {
             return filtered;
