2bb04d1ec4cbabeedd1742177a248bdf1717a6fb
schuang <shanyu.chuang@hitachivantara.com>
2018-07-02 11:42:25 -0700

    Change VSM id format in mock

--- app/mocks/virtualStorageMachineMock.js
+++ app/mocks/virtualStorageMachineMock.js
@@ -16,7 +16,7 @@ rainierAppMock.factory('virtualStorageMachineMock', function (mockUtils) {
         var virtualStorageSystemId = '41000' + v;
         var physicalStorageSystemId = '41000' + _.random(1, 20);
         return {
-            virtualStorageMachineId: 'HM800-220063',
+            virtualStorageMachineId: '220063-HM800',
             storageSystemId: '220063',
             model: 'HM800',
             pairHACount: _.random(1, 50),
